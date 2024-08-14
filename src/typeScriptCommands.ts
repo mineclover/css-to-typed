60aa15074f6a1cdfb9c3cc3df1e8860b864a7bc3
bangjunwoo <mineclover@naver.com>
2024-08-15 01:53:16 +0900

    feat: update Copy Constant Object

--- src/typeScriptCommands.ts
+++ src/typeScriptCommands.ts
@@ -1,6 +1,6 @@
 import * as vscode from "vscode";
 import { inferredTypeMap, currentHoverKey } from "./state";
-
+import { typeToConstantObject } from "./typeUtils";
 export function activate(context: vscode.ExtensionContext) {
   const copyInferredType = vscode.commands.registerCommand(
     "cssToTyped.copyInferredType",
@@ -30,5 +30,34 @@ export function activate(context: vscode.ExtensionContext) {
     }
   );
 
-  context.subscriptions.push(copyInferredType);
+  const copyConstantObject = vscode.commands.registerCommand(
+    "cssToTyped.copyConstantObject",
+    async () => {
+      if (!currentHoverKey) {
+        vscode.window.showErrorMessage("No type to copy");
+        return;
+      }
+
+      const inferredType = inferredTypeMap.get(currentHoverKey);
+      if (inferredType) {
+        try {
+          const constantObject = typeToConstantObject(inferredType);
+          await vscode.env.clipboard.writeText(constantObject);
+          vscode.window.showInformationMessage(
+            "Constant object copied to clipboard"
+          );
+        } catch (error) {
+          vscode.window.showErrorMessage(
+            "Error copying constant object to clipboard"
+          );
+        }
+      } else {
+        vscode.window.showInformationMessage(
+          "No type could be inferred at this position"
+        );
+      }
+    }
+  );
+
+  context.subscriptions.push(copyInferredType, copyConstantObject);
 }
--- src/typeUtils.ts
+++ src/typeUtils.ts
@@ -3,3 +3,14 @@ export const arrayToStringTyped = (arr: string[], name: string) => {
   const context = "export type " + name + "Styles" + " = " + types;
   return context;
 };
+
+export function typeToConstantObject(type: string): string {
+  // Remove whitespace and split the string into key-value pairs
+  const pairs = type.replace(/\s/g, "").slice(1, -1).split(",");
+
+  // Extract only the keys
+  const keys = pairs.map((pair) => pair.split(":")[0]);
+
+  // Create an object with keys
+  return `{ ${keys.join(", ")} }`;
+}
